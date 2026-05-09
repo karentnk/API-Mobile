c8abcfa8a1fcb3236147d1b1e19ab4a17accc1db
Karen Tanaka <160795095+karentnk@users.noreply.github.com>
2026-05-09 18:13:33 -0300

    Create InicioController.java

---
+++ crudproject/src/main/java/com/example/crudproject/controller/controller/InicioController.java
@@ -0,0 +1,13 @@
+package com.example.crudproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InicioController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "API Mobile funcionando";
+    }
+}
